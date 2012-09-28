5fbdd4345ca5d4655ca760c8103b5e3075f113b5
Jonathan Libby <jonathan.libby@yahoo.ca>
2012-09-28 15:19:09 -0400

    General Update
    Added a bunch of comments to the server-side code to clarify on the
    purpose of some things making it easier for other devs to modify the
    code. THIS UPDATE IS NOT GUARANTEED ERROR FREE

--- url.php
+++ url.php
@@ -13,14 +13,14 @@ if(isset($_POST['url'])) {
 		echo 'error_no_url';
 	} else if (filter_var($url, FILTER_VALIDATE_URL) === false) {
 		echo 'error_invalid_url';
-	} else if (is_short($url, $base_url) === true) {
+	} else if (is_short($url, $base_url) === true) { // Is the url already shortened?
 		echo 'error_is_short';
 	} else {
 //	Could probably make this stuff slightly more secure
-		while (!code_exists($code = gen_code($short_charset, $short_length))) {
+		while (!code_exists($code = gen_code($short_charset, $short_length))) { // If the code doesn't already exist, generate one using the configured parameters
 		shorten($url, $code);
 		
-		if($show_protocol == true) {
+		if($show_protocol == true) { // Has the user stated they want the http:// at the beginning of their shortened URL's?
 		echo 'http://' . $base_url . '/' . $code;
 		}
 		if($show_protocol == false) {
